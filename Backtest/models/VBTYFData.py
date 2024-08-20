bdea387dc70f641103ebf83c123fa296be1c7d99
jrab227@gmail.com <jrab227@gmail.com>
2024-08-20 14:07:03 -0400

    Updating older Backtest report.

--- Backtest/models/VBTYFData.py
+++ Backtest/models/VBTYFData.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 from Backtest.models.LocalDataStorage import LocalDataStore
 import vectorbt as vbt
@@ -10,6 +11,24 @@ class VBTYFData(LocalDataStore):
 
     def __init__(self, file_path: str):
         super().__init__(file_path)
+
+    def load(self, *args, **kwargs) -> pd.DataFrame:
+        """
+        Load the data from a file or fetch it if the file does not exist.
+        Parameters:
+            **kwargs: Additional keyword arguments to be passed to the fetch method.
+        Returns:
+            pd.DataFrame: The loaded or fetched data as a pandas DataFrame.
+        """
+        if os.path.exists(self.file_path):
+            df = pd.read_csv(self.file_path, index_col=0, parse_dates=True)
+            df = df["Close"]
+        else:
+            df = self.fetch(*args, **kwargs)
+            df.to_csv(self.file_path)
+        
+        self.data = df
+        return df
 
 
     def fetch(self, ticker, debug=False, **kwargs) -> pd.DataFrame:
